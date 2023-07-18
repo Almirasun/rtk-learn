3c4d10968fb21d0f5f32d1170182cc9ba07ba6c6
Almirasun <almira.sagynbek@gmail.com>
2023-07-18 19:25:20 +0600

    added middleware

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,21 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { reducer as favoritesReducer } from "./favorites/favorites.slice";
 import { api } from "./api/api";
+// import { userSlice } from "./user/user.slice";
+import { createLogger } from "redux-logger";
+
+const logger = createLogger({
+  collapsed: true,
+});
 
 const reducers = combineReducers({
   favorites: favoritesReducer,
+  // user: userSlice.reducer,
   [api.reducerPath]: api.reducer,
 });
 
 export const store = configureStore({
   reducer: reducers,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(api.middleware).concat(logger),
 });
